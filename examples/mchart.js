8642e261278dba08f30ca513a592559e18b69037
Moyu <492899414@qq.com>
2017-08-05 14:59:57 +0800

    better editable

--- examples/mchart.js
+++ examples/mchart.js
@@ -3,22 +3,84 @@ import ReactDOM from 'react-dom'
 import {Histogram} from 'react-mchart'
 import 'react-mchart/css/style.css'
 
+import {
+    observable
+} from 'mobx'
+import {
+    observer
+} from 'mobx-react'
 
 import editable from '../lib/editable'
 import {stringify} from '../lib/helper'
 
 
-const EnhancedExample = editable({
-    functionDetail: true, infoDown: false,
-    attrNames: ['local', 'state']
+const EnhancedExampleA = editable(Histogram)
+
+const EnhancedExampleB = editable({
+    attrNames: ['state'], groupName: 'groupB'
 })(Histogram)
 
+
+function Person(a, b) {
+    this.name = a;
+    this.age = b;
+}
+
+class State {
+    @observable foo = {a: 234}
+    @observable bar = "bar"
+}
+
+@editable({
+    attrNames: ['local']
+})
+@observer
+class MobxTest extends React.Component {
+    local = new State
+
+    render() {
+        return (
+            <div>
+                <h1>MobxTest</h1>
+                <pre>{stringify(this.local)}</pre>
+                <div>{this.props.children}</div>
+            </div>
+        )
+    }
+}
+
+class Container extends React.Component {
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                hide: true
+            })
+        }, 2000)
+    }
+
+    state = {hide: false}
+
+    render() {
+        return (
+            <div>
+                <EnhancedExampleA
+                    b={true}
+                    person={new Person('my', 19)}
+                    Person={Person}
+                    Ele={EnhancedExampleA}
+                    ele={<EnhancedExampleA/>}
+                />
+                <EnhancedExampleB />
+                {!this.state.hide && <EnhancedExampleB />}
+                <MobxTest />
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <EnhancedExample
-        func={() => console.log(this)}
-        null={null}
-        num={0}
-    />,
+    <Container/>,
     document.getElementById('root')
 )
 
