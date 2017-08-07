3e97fac4f333f1a1d378d5ca124142d33b5f1413
Moyu <492899414@qq.com>
2017-08-07 23:16:57 +0800

    update page

--- examples/Histogram/Tile.js
+++ examples/Histogram/Tile.js
@@ -7,41 +7,46 @@ import {Histogram} from 'react-mchart'
 
 
 export default class TileHistogram extends React.Component {
+
+    static defaultProps = {
+        xAxis: [
+            {
+                value: '百度',
+                style: {
+                    // color: 'red'
+                }
+            },
+            '谷歌'
+        ],
+        animate: true,
+        data: [
+            [
+                {
+                    value: 12,
+                    // color: 'blue',
+                    label: '上半年',
+                    onClick: () => alert('hello')
+                },
+                {
+                    value: 11,
+                    color: 'rgb(249, 204, 157)',
+                    label: '下半年',
+                    onClick: () => alert('world')
+                },
+            ],
+            {
+                value: 14,
+                // color: 'green'
+            }
+        ],
+        rectWidth: 35,
+        height: 250
+    }
+
     render() {
         return (
             <Histogram
-                xAxis={[
-                    {
-                        value: '百度',
-                        style: {
-                            // color: 'red'
-                        }
-                    },
-                    '谷歌'
-                ]}
-                animate
-                data={[
-                    [
-                        {
-                            value: 12,
-                            // color: 'blue',
-                            label: '上半年',
-                            onClick: () => alert('hello')
-                        },
-                        {
-                            value: 11,
-                            color: 'rgb(249, 204, 157)',
-                            label: '下半年',
-                            onClick: () => alert('world')
-                        },
-                    ],
-                    {
-                        value: 14,
-                        // color: 'green'
-                    }
-                ]}
-                rectWidth={35}
-                height={250}
+                {...this.props}
             />
         )
     }
